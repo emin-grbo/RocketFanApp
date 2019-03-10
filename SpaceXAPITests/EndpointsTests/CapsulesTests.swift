a971c262af926d6c315a5a8f64c8558c0325c25a
Robert J. Clegg <Robert.clegg@gmail.com>
2019-03-10 21:52:50 +0200

    Added Capsules endpoint with unit tests

---
+++ SpaceXAPITests/EndpointsTests/CapsulesTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+@testable import SpaceXAPI
+
+class CapsulesTests: XCTestCase {
+
+    func test_Capsules_All_ReturnsPath() {
+        let path = Capsules.all.path
+
+        XCTAssertEqual(path, "/capsules")
+    }
+
+    func test_Capsules_One_ReturnsPath() {
+        let identifier = "C223"
+        let path = Capsules.one(identifier).path
+
+        XCTAssertEqual(path, "/capsules/\(identifier)")
+    }
+
+    func test_Capsules_Upcoming_ReturnsPath() {
+        let path = Capsules.upcoming.path
+
+        XCTAssertEqual(path, "/capsules/upcoming")
+    }
+
+    func test_Capsules_Past_ReturnsPath() {
+        let path = Capsules.past.path
+
+        XCTAssertEqual(path, "/capsules/past")
+    }
+}
