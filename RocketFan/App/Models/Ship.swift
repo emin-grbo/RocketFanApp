263a4642bbb094fd8eff7f21013a3d4f58bb0a8c
Paul Williamson <squarefrog@gmail.com>
2019-03-31 11:11:28 +0100

    Audit Ship
    See #21

--- RocketFan/App/Models/Ship.swift
+++ RocketFan/App/Models/Ship.swift
@@ -1,9 +1,10 @@
 import Foundation
 
 struct Ship: Decodable {
-    typealias Weight = Units
+    typealias Degrees = Double
+    typealias Knots = Double
 
-    let courseDeg: Int?
+    let course: Degrees?
     let homePort: String
     let id: String
     let identifiers: Identifiers?
@@ -15,10 +16,11 @@ struct Ship: Decodable {
     let model: String?
     let name: String
     let roles: [String]
-    let speed: Speed?
+    let speed: Knots?
     let status: String?
+    let type: String
     let url: URL?
-    let weight: Weight?
+    let weight: Units?
     let yearBuilt: Int?
 }
 
@@ -41,6 +43,7 @@ extension Ship {
         case speedKn = "speed_kn"
         case status
         case successfulLandings = "successful_landings"
+        case type = "ship_type"
         case url
         case weightKg = "weight_kg"
         case weightLbs = "weight_lbs"
@@ -50,7 +53,7 @@ extension Ship {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        courseDeg = try container.decodeIfPresent(Int.self, forKey: .courseDeg)
+        course = try container.decodeIfPresent(Double.self, forKey: .courseDeg)
         homePort = try container.decode(String.self, forKey: .homePort)
         id = try container.decode(String.self, forKey: .id)
         imageUrl = try container.decodeIfPresent(URL.self, forKey: .imageUrl)
@@ -60,16 +63,15 @@ extension Ship {
         model = try container.decodeIfPresent(String.self, forKey: .model)
         name = try container.decode(String.self, forKey: .name)
         roles = try container.decode([String].self, forKey: .roles)
+        speed = try container.decodeIfPresent(Knots.self, forKey: .speedKn)
         status = try container.decodeIfPresent(String.self, forKey: .status)
+        type = try container.decode(String.self, forKey: .type)
         url = try container.decodeIfPresent(URL.self, forKey: .url)
         yearBuilt = try container.decodeIfPresent(Int.self, forKey: .yearBuilt)
 
-        let kn = try container.decodeIfPresent(Double.self, forKey: .speedKn)
-        speed = Speed(kn: kn)
-
         let lbs = try container.decodeIfPresent(Double.self, forKey: .weightLbs)
         let kg = try container.decodeIfPresent(Double.self, forKey: .weightKg)
-        weight = Weight(metric: kg, imperial: lbs)
+        weight = Units(metric: kg, imperial: lbs)
 
         let attempts = try container.decodeIfPresent(Int.self, forKey: .attemptedLandings)
         let successes = try container.decodeIfPresent(Int.self, forKey: .successfulLandings)
