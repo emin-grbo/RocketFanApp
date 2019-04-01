e6ae672889ae64211908a755685e31876e75568f
Robert J. Clegg <Robert.clegg@gmail.com>
2019-04-01 17:23:36 +0200

    Removed properties from Roadster that we probably won't need

--- RocketFan/App/Models/Roadster.swift
+++ RocketFan/App/Models/Roadster.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 struct Roadster: Decodable {
-    let astronomicalUnits: AstronomicalUnits
     let daysInSpace: Double
     let details: String
 
@@ -10,10 +9,6 @@ struct Roadster: Decodable {
 
     /// Specified in KM and Miles
     let distanceFromMars: Units
-
-    let eccentricity: Double
-    let epochJd: Double
-    let inclination: Double
     let launchDate: Date
 
     /// Specified in Kg and Lbs
@@ -21,9 +16,6 @@ struct Roadster: Decodable {
 
     let links: Links
     let name: String
-    let noradId: Int
-    let orbitType: String
-    let periapsisOrg: Double
 
     /// Specified in Kph and Mph
     let speed: Units
@@ -33,25 +25,12 @@ struct Roadster: Decodable {
 
         name = try container.decode(String.self, forKey: .name)
         launchDate = try container.decode(Date.self, forKey: .launchDate)
-        eccentricity = try container.decode(Double.self, forKey: .eccentricity)
-        inclination = try container.decode(Double.self, forKey: .inclination)
-        periapsisOrg = try container.decode(Double.self, forKey: .periapsisOrg)
         daysInSpace = try container.decode(Double.self, forKey: .daysInSpace)
-        noradId = try container.decode(Int.self, forKey: .noradId)
-        epochJd = try container.decode(Double.self, forKey: .epochJd)
-        orbitType = try container.decode(String.self, forKey: .orbitType)
         details = try container.decode(String.self, forKey: .details)
 
         let kg = try container.decode(Double.self, forKey: .launchMassKg)
         let lbs = try container.decode(Double.self, forKey: .launchMassLbs)
         launchMass = Units(metric: kg, imperial: lbs)!
-
-        let apoapsis = try container.decode(Double.self, forKey: .apoapsis)
-        let periapsis = try container.decode(Double.self, forKey: .periapsis)
-        let semiMajorAxis = try container.decode(Double.self, forKey: .semiMajorAxis)
-        astronomicalUnits = AstronomicalUnits(apoapsis: apoapsis,
-                                              periapsis: periapsis,
-                                              semiMajorAxis: semiMajorAxis)
 
         let kph = try container.decode(Double.self, forKey: .kph)
         let mph = try container.decode(Double.self, forKey: .mph)
@@ -71,14 +50,10 @@ struct Roadster: Decodable {
 
 extension Roadster {
     enum CodingKeys: String, CodingKey {
-        case apoapsis = "apoapsis_au"
         case daysInSpace = "period_days"
         case details
         case earthKm = "earth_distance_km"
         case earthMi = "earth_distance_mi"
-        case eccentricity
-        case epochJd = "epoch_jd"
-        case inclination
         case kph = "speed_kph"
         case launchDate = "launch_date_unix"
         case launchMassKg = "launch_mass_kg"
@@ -87,28 +62,6 @@ extension Roadster {
         case marsMi = "mars_distance_mi"
         case mph = "speed_mph"
         case name
-        case noradId = "norad_id"
-        case orbitType = "orbit_type"
-        case periapsis = "periapsis_au"
-        case periapsisOrg = "periapsis_arg"
-        case semiMajorAxis = "semi_major_axis_au"
         case wikipedia
-    }
-}
-
-extension Roadster {
-    struct AstronomicalUnits {
-        let apoapsis: Double
-        let periapsis: Double
-        let semiMajorAxis: Double
-
-        init(apoapsis: Double,
-             periapsis: Double,
-             semiMajorAxis: Double) {
-
-            self.apoapsis = apoapsis
-            self.periapsis = periapsis
-            self.semiMajorAxis = semiMajorAxis
-        }
     }
 }
