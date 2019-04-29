e2722930077cc4fb5324c85132bc1f2f4accb5f2
Robert J. Clegg <Robert.clegg@gmail.com>
2019-04-29 15:55:59 +0200

    Removed APIFactory

--- RocketFan/Dependency Containers/DependencyContainer.swift
+++ RocketFan/Dependency Containers/DependencyContainer.swift
@@ -1,12 +1,12 @@
 import Foundation
 import SpaceXAPI
 
-class DependencyContainer: ViewControllerFactory {
-
-}
-
-extension DependencyContainer: APIFactory {
+class DependencyContainer {
     func makeSpaceXAPI() -> SpaceXAPI {
         return SpaceXAPI(urlSession: URLSession.shared)
     }
+}
+
+extension DependencyContainer: ViewControllerFactory {
+
 }
