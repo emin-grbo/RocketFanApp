15b5f49ea4e5070ca2be420eb9346fe5154826c8
Paul Williamson <squarefrog@gmail.com>
2019-05-02 19:42:53 +0100

    Add LaunchCell for displaying launch info

---
+++ RocketFan/Feature/Launches/All/LaunchCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class LaunchCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var launchPadLabel: UILabel!
+    @IBOutlet weak var rocketLabel: UILabel!
+    @IBOutlet weak var patchImageView: UIImageView!
+}
