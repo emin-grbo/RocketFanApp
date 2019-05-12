34dbea1e4bad46ff422ba1b7d8315284f21d6b25
Paul Williamson <squarefrog@gmail.com>
2019-05-12 16:49:38 +0100

    Add YouTubeViewController and WebKit framework

---
+++ RocketFan/Feature/Launches/Details/YouTubeViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+import WebKit
+
+class YouTubeViewController: UIViewController {
+    init(videoIdentifier: String) {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
