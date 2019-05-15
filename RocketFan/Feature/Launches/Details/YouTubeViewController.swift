f11c39ac36446d8f2f8848e3b2de89d6a3ac4c0c
Paul Williamson <squarefrog@gmail.com>
2019-05-15 22:23:33 +0100

    Add WebView and request loading to YouTubeViewController

--- RocketFan/Feature/Launches/Details/YouTubeViewController.swift
+++ RocketFan/Feature/Launches/Details/YouTubeViewController.swift
@@ -2,12 +2,24 @@ import UIKit
 import WebKit
 
 class YouTubeViewController: UIViewController {
-    init(videoIdentifier: String) {
+    @IBOutlet weak var webView: WKWebView!
+
+    private let videoURL: URL
+
+    init(videoURL: URL) {
+        self.videoURL = videoURL
+
         super.init(nibName: nil, bundle: nil)
     }
 
     @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        webView.load(URLRequest(url: videoURL))
     }
 }
