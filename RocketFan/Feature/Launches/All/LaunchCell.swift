1a63590df394b5588167e378c09f775a5f278400
Robert J. Clegg <Robert.clegg@gmail.com>
2019-05-23 21:48:09 +0200

    Changes made to remove reference to VM in cell

--- RocketFan/Feature/Launches/All/LaunchCell.swift
+++ RocketFan/Feature/Launches/All/LaunchCell.swift
@@ -6,23 +6,19 @@ class LaunchCell: UITableViewCell {
     @IBOutlet private weak var launchPadLabel: UILabel!
     @IBOutlet private weak var rocketLabel: UILabel!
     @IBOutlet private weak var patchImageView: UIImageView!
-
-    private var viewModel: LaunchCellViewModel?
 }
 
 extension LaunchCell {
     func configure(with viewModel: LaunchCellViewModel) {
-        self.viewModel = viewModel
-
-        setupLabels()
+        setupLabels(with: viewModel)
     }
 }
 
 extension LaunchCell {
-    private func setupLabels() {
-        titleLabel.text = viewModel?.missionName
-        dateLabel.text = viewModel?.launchDate
-        launchPadLabel.text = viewModel?.siteShortName
-        rocketLabel.text = viewModel?.rocketName
+    private func setupLabels(with viewModel: LaunchCellViewModel) {
+        titleLabel.text = viewModel.missionName
+        dateLabel.text = viewModel.launchDate
+        launchPadLabel.text = viewModel.siteShortName
+        rocketLabel.text = viewModel.rocketName
     }
 }
