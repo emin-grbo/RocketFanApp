a80689f028d13baa4358923b65077a79b13171fa
Robert J. Clegg <Robert.clegg@gmail.com>
2019-05-23 21:33:13 +0200

    LaunchCell implementation

--- RocketFan/Feature/Launches/All/LaunchCell.swift
+++ RocketFan/Feature/Launches/All/LaunchCell.swift
@@ -6,4 +6,23 @@ class LaunchCell: UITableViewCell {
     @IBOutlet private weak var launchPadLabel: UILabel!
     @IBOutlet private weak var rocketLabel: UILabel!
     @IBOutlet private weak var patchImageView: UIImageView!
+
+    private var viewModel: LaunchCellViewModel?
+}
+
+extension LaunchCell {
+    func configure(with viewModel: LaunchCellViewModel) {
+        self.viewModel = viewModel
+
+        setupLabels()
+    }
+}
+
+extension LaunchCell {
+    private func setupLabels() {
+        titleLabel.text = viewModel?.missionName
+        dateLabel.text = viewModel?.launchDate
+        launchPadLabel.text = viewModel?.siteShortName
+        rocketLabel.text = viewModel?.rocketName
+    }
 }
