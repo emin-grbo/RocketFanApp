b25a7d3267aba32c77aedcc5621c6a970dfe4f27
Robert J. Clegg <Robert.clegg@gmail.com>
2019-05-25 16:24:52 +0200

    Added implementation for launch cell configure

--- RocketFan/Feature/Launches/All/LaunchTableViewController.swift
+++ RocketFan/Feature/Launches/All/LaunchTableViewController.swift
@@ -1,4 +1,15 @@
 import UIKit
 
 class LaunchTableViewController: UITableViewController {
+    private let dataSource: LaunchTableViewDataSource
+
+    init(with launches: [Launch]) {
+        dataSource = LaunchTableViewDataSource(with: launches)
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
