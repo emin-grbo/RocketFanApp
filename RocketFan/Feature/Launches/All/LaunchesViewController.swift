55d9184be4fb1c6ebf39ef1aafbb71ba0115ed0c
Robert J. Clegg <Robert.clegg@gmail.com>
2019-06-01 11:10:52 +0200

    Added search UI

--- RocketFan/Feature/Launches/All/LaunchesViewController.swift
+++ RocketFan/Feature/Launches/All/LaunchesViewController.swift
@@ -16,7 +16,6 @@ class LaunchesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .white
         add(contentStateViewController)
         bindViewModel()
@@ -29,11 +28,49 @@ extension LaunchesViewController {
             self?.handleSuccess(with: launches)
         }
     }
-
     private func handleSuccess(with launches: [Launch]) {
         DispatchQueue.main.async { [weak self] in
-            let launchesVC = LaunchTableViewController(with: launches)
-            self?.contentStateViewController.transition(to: .render(launchesVC))
+            self?.setupLoadedState(with: launches)
         }
+    }
+
+    private func setupLoadedState(with launches: [Launch]) {
+        title = "Launches"
+        setupeNavigationBar()
+
+        let launchesVC = LaunchTableViewController(with: launches)
+        contentStateViewController.transition(to: .render(launchesVC))
+    }
+
+    private func setupeNavigationBar() {
+        navigationItem.searchController = setupSearchController()
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    private func setupSearchController() -> UISearchController {
+        let searchController = UISearchController(searchResultsController: nil)
+
+        searchController.searchBar.placeholder = "Search Launches"
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+
+        setupScopeBar(for: searchController.searchBar)
+
+        return searchController
+    }
+
+    private func setupScopeBar(for searchBar: UISearchBar) {
+        searchBar.delegate = self
+        searchBar.scopeButtonTitles = ["Past", "Upcoming"]
+    }
+}
+
+extension LaunchesViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+    }
+}
+
+extension LaunchesViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
     }
 }
