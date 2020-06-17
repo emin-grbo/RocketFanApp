b10d0e3e1a33716f7fe972b06bbfcf54c241686b
roblack <emin.roblack@gmail.com>
2020-06-17 22:55:30 +0200

    feat: add separate asset catalogues and define Theme enum

---
+++ RocketFan/Utilities/Theme.swift
@@ -0,0 +1,17 @@
+//
+//  Theme.swift
+//  RocketFan
+//
+//  Created by Emin Roblack on 17/06/2020.
+//  Copyright © 2020 RocketFanOrg. All rights reserved.
+//
+
+import SwiftUI
+
+enum Theme {
+    var background:     Color {     Color("background") }
+    var darkVariant:    Color {     Color("darkVariant") }
+    var lightVariant:   Color {     Color("lightVariant") }
+    var primary:        Color {     Color("primary") }
+    var secondary:      Color {     Color("secondary") }
+}
