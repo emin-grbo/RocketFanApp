f9bebf26f6bebd898d1d22e4388318f5b4b1bfcf
emiN <emin.grbo@telego.rs>
2020-06-18 12:48:38 +0200

    chore: fix formating and remove header

--- RocketFan/Utilities/Theme.swift
+++ RocketFan/Utilities/Theme.swift
@@ -1,17 +1,9 @@
-//
-//  Theme.swift
-//  RocketFan
-//
-//  Created by Emin Roblack on 17/06/2020.
-//  Copyright © 2020 RocketFanOrg. All rights reserved.
-//
-
 import SwiftUI
 
 enum Theme {
-    static var background:     Color {     Color("background") }
-    static var darkVariant:    Color {     Color("darkVariant") }
-    static var lightVariant:   Color {     Color("lightVariant") }
-    static var primary:        Color {     Color("primary") }
-    static var secondary:      Color {     Color("secondary") }
+    static var background: Color { Color("background") }
+    static var darkVariant: Color { Color("darkVariant") }
+    static var lightVariant: Color { Color("lightVariant") }
+    static var primary: Color { Color("primary") }
+    static var secondary: Color { Color("secondary") }
 }
